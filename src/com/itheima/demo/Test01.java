2bb898a1ab466198426db4886120efe897b1eb46
satire <l>
2020-07-01 19:40:55 +0800

    第一次修改

--- src/com/itheima/demo/Test01.java
+++ src/com/itheima/demo/Test01.java
@@ -3,5 +3,6 @@ package com.itheima.demo;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
